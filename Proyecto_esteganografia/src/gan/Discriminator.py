5de1de751d4bc07bb04a9093804fbc7d5bf380c5
noraib <nora.ibarguren@opendeusto.es>
2026-03-27 11:16:25 +0100

    cambios de directorios del modulo de gan

--- Proyecto_esteganografia/src/gan/Discriminator.py
+++ Proyecto_esteganografia/src/gan/Discriminator.py
@@ -2,23 +2,26 @@ import torch
 import torch.nn as nn
 
 class Discriminator(nn.Module):
-    def __init__(self, img_channels=3, message_size=128):
+    def __init__(self, img_channels=3):
         super(Discriminator, self).__init__()
         self.img_channels = img_channels
-        self.message_size = message_size
 
         self.conv_blocks = nn.Sequential(
+            # Entrada: (3, 64, 64) -> Salida: (64, 32, 32)
             nn.Conv2d(img_channels, 64, kernel_size=4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
 
+            # Entrada: (64, 32, 32) -> Salida: (128, 16, 16)
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
 
+            # Entrada: (128, 16, 16) -> Salida: (256, 8, 8)
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
 
+            # Entrada: (256, 8, 8) -> Salida: (512, 4, 4)
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True)
@@ -29,19 +32,13 @@ class Discriminator(nn.Module):
 
         # Cabeza Real/Falso
         self.fc_real_fake = nn.Sequential(
+            # 512 canales * 4 alto * 4 ancho = 8192 neuronas de entrada
             nn.Linear(512*4*4, 1),
-            nn.Sigmoid()
-        )
-
-        # Cabeza de decodificación de mensaje
-        self.fc_message = nn.Sequential(
-            nn.Linear(512*4*4, message_size),
-            nn.Sigmoid()
+            nn.Sigmoid() # Devuelve una probabilidad entre 0 (falso) y 1 (real)
         )
 
     def forward(self, img):
         x = self.conv_blocks(img)
         x = self.flatten(x)
         real_fake = self.fc_real_fake(x)
-        message_hat = self.fc_message(x)
-        return real_fake, message_hat
+        return real_fake
--- Proyecto_esteganografia/src/gan/Generator.py
+++ Proyecto_esteganografia/src/gan/Generator.py
@@ -2,21 +2,17 @@ import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
-    def __init__(self, z_dim=100, message_size=128, img_channels=3, feature_map_size=64):
+    def __init__(self, z_dim=100, img_channels=3, feature_map_size=64):
         super(Generator, self).__init__()
 
         # Parametros 
         self.z_dim = z_dim # Ruido
-        self.message_size = message_size # Bits del mensaje oculto
-        self.input_dim = z_dim + message_size  # Concatenamos z (ruido) + mensaje
-        self.img_channels = img_channels
-        self.feature_map_size = feature_map_size
+        self.img_channels = img_channels # Canales de la imagen (RGB)
 
         # PARTE 1: Fully Connected 
         # Empezamos con una imagen pequeña (4x4 pixeles)
         self.net = nn.Sequential(
-            # z+m -> 4x4x512
-            nn.Linear(self.input_dim, 512*4*4),
+            nn.Linear(self.z_dim, 512*4*4),
             nn.BatchNorm1d(512*4*4),
             nn.ReLU(True)
         )
@@ -24,32 +20,33 @@ class Generator(nn.Module):
         # PARTE 2: Upsampling con ConvTranspose2d
         # Vamos aumentando el tamaño de la imagen (el doble por cada salto)
         self.deconv_blocks = nn.Sequential(
+            # Entrada: (512, 4, 4) -> Salida: (256, 8, 8)
             nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
 
+            # Entrada: (256, 8, 8) -> Salida: (128, 16, 16)
             nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
 
+            # Entrada: (128, 16, 16) -> Salida: (64, 32, 32)
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
+            # Entrada: (64, 32, 32) -> Salida: (img_channels, 64, 64)
             nn.ConvTranspose2d(64, self.img_channels, kernel_size=4, stride=2, padding=1),
             nn.Tanh()  # salida en [-1,1]
         )
 
-    def forward(self, z, message):
+    def forward(self, z):
         """
         z: tensor de ruido (batch_size, z_dim)
-        message: tensor binario (batch_size, message_size)
         """
-        # Concatenamos z + message
-        x = torch.cat([z, message], dim=1)  # shape: (batch_size, z_dim+message_size)
-
+        
         # Fully connected
-        x = self.net(x)  # shape: (batch_size, 512*4*4)
+        x = self.net(z)  # shape: (batch_size, 512*4*4)
         x = x.view(-1, 512, 4, 4)  # reshape para ConvTranspose2d
 
         # Upsampling
